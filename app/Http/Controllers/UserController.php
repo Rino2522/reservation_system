5c7da2edaf87257b92b94abc37ab4db8cdb2eff4
rino2522 <2211430017u@nara.kindai.ac.jp>
2025-02-21 09:32:35 +0000

    users-index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reservation;
+use Carbon\Carbon;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return view('users.index');
-    }
+        $today = Carbon::today();
+        $reservations = Reservation::where('date_time', '>=', $today)
+                                   ->orderBy('date_time', 'asc')
+                                   ->get();
+        return view('users.index')->with('reservations', $reservations);
+    }  
 }
