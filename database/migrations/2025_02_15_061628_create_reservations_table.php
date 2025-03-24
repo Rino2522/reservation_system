ca0201b02027603c4c4b87d10d07ff7bf4a31fef
あなたの名前Run <>
2025-03-24 04:22:13 +0000

    addition3

--- database/migrations/2025_02_15_061628_create_reservations_table.php
+++ database/migrations/2025_02_15_061628_create_reservations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->nullable();
+            $table->string('email')->nullable(false);
             $table->string('phone',15);
             $table->integer('number_of_guests');
             $table->date('date');
